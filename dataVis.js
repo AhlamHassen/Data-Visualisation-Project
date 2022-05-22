6007513c252f5693c14ca591ec32d25d76e165f9
AhlamHassen <ahlamsaleh199700@gmail.com>
2022-05-22 19:27:37 +1000

    Added tooltip, legend and legend interactivity

--- dataVis.js
+++ dataVis.js
@@ -1,10 +1,11 @@
 function init() {
-    var formatDate, dataset, header, xScale, yScale, xAxis, yAxis, svg, coalConsLine;
+    var formatDate, dataset, header, xScale, yScale, xAxis, yAxis, svg, coalConsLine,
+        gasConsLine, hydroConsLine, oilConsLine, solarConsLine, windConsLine, toolTip, lineData;
 
     //dimensions
     const margin = { top: 30, right: 40, bottom: 70, left: 20 };
     const width = 650 - margin.right - margin.left;
-    const height = 530 - margin.top - margin.bottom;
+    const height = 520 - margin.top - margin.bottom;
 
     //format date
     formatDate = d3.timeFormat("%Y");
@@ -18,32 +19,20 @@ function init() {
             //coal consumption
             coalCons: parseFloat(d.coal_consumption),
 
-            //coal production
-            coalProd: parseFloat(d.coal_production),
-
-            //electricity generation
-            elecPrdo: parseFloat(d.electricity_generation),
-
-            //gas consumption
-            gasCons: parseFloat(d.gas_consumption),
-
-            //gas production
-            gasProd: parseFloat(d.gas_production),
-
-            //hydro consumption
-            hydroCons: parseFloat(d.hydro_consumption),
-
             //oil consumption
             oilCons: parseFloat(d.oil_consumption),
 
-            //oil production
-            oilProd: parseFloat(d.oil_production),
+            //gas consumption
+            gasCons: parseFloat(d.gas_consumption),
 
             //solar energy consumption
             solarCons: parseFloat(d.solar_consumption),
 
             //wind energy consumption
-            windCons: parseFloat(d.wind_consumption)
+            windCons: parseFloat(d.wind_consumption),
+
+            //hydro consumption
+            hydroCons: parseFloat(d.hydro_consumption)
         }
     }).then(function (data) {
 
@@ -51,12 +40,12 @@ function init() {
         MultiLineChart(dataset);
 
         //to check if the dataset has been imported
-        //console.table(dataset, ["date", "coalCons", "coalProd", "elecProd", "gasCons", "gasProd", "hydroCons", "oilCons", "oilProd", "solarCons", "windCons"]);
+        console.table(dataset, ["date", "coalCons", "coalProd", "elecProd", "gasCons", "gasProd", "hydroCons", "oilCons", "oilProd", "solarCons", "windCons"]);
     });
 
     function MultiLineChart() {
         //chart base
-        svg = d3.select("body")
+        svg = d3.select("#chartContainer")
             .append("svg")
             .attr("width", width + margin.right + margin.left)
             .attr("height", height + margin.top + margin.bottom)
@@ -79,6 +68,7 @@ function init() {
             .attr("x", 0)
             .attr("y", 15)
             .text("Measured in Terawatt hour (TWh) from 2000-2019")
+            .style('fill', 'grey');
 
 
         //x scale setup
@@ -108,105 +98,274 @@ function init() {
             .ticks(10)
             .scale(yScale);
 
+        //an array to hold all of the property names
+        var propertyNames = [];
+        for (var name in dataset[0]) {
+            if (name == "date") {
+                continue;
+            }
+
+            propertyNames.push(name);
+        }
+
+        //colors 
+        //var colors = d3.schemeCategory10;
+        var colors = ["#000000", "#e69f00", "#56b4e9", "#ff0000", "#f0e442", "#4b0092"];
+
+        //draw each line with different color
+        drawAllLines();
+
         //call on x axis
         svg.append("g")
             .attr("class", "axis")
-            .attr("class", "Xaxis")
             .attr("transform", "translate(" + margin.left + "," + height + ")") //place x axes on the bottom
             .call(xAxis);
 
         //call on y axis
         svg.append("g")
-            .attr("class", "axis")
+            .attr("class", "axis yAxis")
             .attr("transform", "translate(" + margin.left + ", 0)") //place y axis on the left side
             .call(yAxis);
 
-        //coal energy consumption line genetator        
-        coalConsLine = d3.line()
-            .x(function (d) { return xScale(d.date); })
-            .y(function (d) { return yScale(d.coalCons); });
+        //draws all the lines
+        function drawLines(pName, color) {
+            //energy consumption line genetator        
+            consLine = d3.line()
+                .x(function (d) { return xScale(d.date); })
+                .y(function (d) { return yScale(d[pName]); });
 
-        //Creating the coal energy consumption line
-        svg.append("path")
-            .datum(dataset)
-            .attr("class", "coalConsLine")
-            .attr("class", "consLines")
-            .attr("transform", "translate(" + margin.left + ", 0)")
-            .attr("d", coalConsLine)
-            .style("stroke", "#010E13");
+            //Creating the energy consumption line
+            svg.append("path")
+                .datum(dataset)
+                .attr("class", "consLines " + pName + "Line")
+                .attr("transform", "translate(" + margin.left + ", 0)")
+                .attr("d", consLine)
+                .style("stroke", color)
+        }
 
-        //gas energy consumption line genetator        
-        gasConsLine = d3.line()
-            .x(function (d) { return xScale(d.date); })
-            .y(function (d) { return yScale(d.gasCons); });
+        function drawAllLines() {
+            for (var i = 0; i < propertyNames.length; i++) {
+                drawLines(propertyNames[i], colors[i]);
+            }
+        }
 
-        //Creating the gas energy consumption line
-        svg.append("path")
-            .datum(dataset)
-            .attr("class", "gasConsLine")
-            .attr("class", "consLines")
-            .attr("transform", "translate(" + margin.left + ", 0)")
-            .attr("d", gasConsLine)
-            .style("stroke", "#ffa600");
+        //group for the hover - vertical line and tooltip
+        var hoverLineGroup = svg.append("g")
+            .attr("class", "hover-line")
+            .attr("transform", "translate(" + margin.left + ", 0)");
 
-        //hydro energy consumption line genetator        
-        hydroConsLine = d3.line()
-            .x(function (d) { return xScale(d.date); })
-            .y(function (d) { return yScale(d.hydroCons); });
+        //vertical line
+        var hoverLine = hoverLineGroup
+            .append("line")
+            .attr("stroke", "#000")
+            .attr("y1", 0)
+            .attr("y2", height);
 
-        //Creating the hydro energy consumption line
-        svg.append("path")
-            .datum(dataset)
-            .attr("class", "hydroConsLine")
-            .attr("class", "consLines")
-            .attr("transform", "translate(" + margin.left + ", 0)")
-            .attr("d", hydroConsLine)
-            .style("stroke", "#EF3E36");
+        //define tooltip
+        toolTip = d3.selectAll(".toolTip");
 
-        //Oil energy consumption line genetator        
-        oilConsLine = d3.line()
-            .x(function (d) { return xScale(d.date); })
-            .y(function (d) { return yScale(d.oilCons); });
+        //invisible when no mouse events
+        hoverLineGroup.style("opacity", 1e-6);
 
-        //Creating the oil energy consumption line
-        svg.append("path")
-            .datum(dataset)
-            .attr("class", "oilConsLine")
-            .attr("class", "consLines")
-            .attr("transform", "translate(" + margin.left + ", 0)")
-            .attr("d", oilConsLine)
-            .style("stroke", "#FFF275");
+        //rectangular area of hover
+        var rectHover = svg.append("rect")
+            .data(dataset)
+            .attr("fill", "none")
+            .attr("class", "overlay")
+            .attr("width", width + margin.left)
+            .attr("height", height + margin.top);
 
-        //Solar energy consumption line genetator        
-        solarConsLine = d3.line()
-            .x(function (d) { return xScale(d.date); })
-            .y(function (d) { return yScale(d.solarCons); });
+        //functions for hover events
+        svg.on("mouseout", mouseout)
+            .on("mousemove", hoverMouseOn);
 
-        //Creating the oil energy consumption line
-        svg.append("path")
-            .datum(dataset)
-            .attr("class", "solarConsLine")
-            .attr("class", "consLines")
-            .attr("transform", "translate(" + margin.left + ", 0)")
-            .attr("d", solarConsLine)
-            .style("stroke", "#0B6E4F");
+        //gets only date
+        var bisectDate = d3.bisector(function (d) { return d.date; }).left;
 
-        //Wind energy consumption line genetator        
-        windConsLine = d3.line()
-            .x(function (d) { return xScale(d.date); })
-            .y(function (d) { return yScale(d.windCons); });
+        function hoverMouseOn() {
+            //get x and y mouse positions
+            var mouse_x = d3.mouse(this)[0];
+            var mouse_y = d3.mouse(this)[1];
 
-        //Creating the wind energy consumption line
-        svg.append("path")
-            .datum(dataset)
-            .attr("class", "windConsLine")
-            .attr("class", "consLines")
-            .attr("transform", "translate(" + margin.left + ", 0)")
-            .attr("d", windConsLine)
-            .style("stroke", "#17BEBB");
+            //actual x value
+            var mouseDate = xScale.invert(mouse_x);
+
+            //returns the index to the current data item
+            var i = bisectDate(dataset, mouseDate);
+            var d0 = dataset[i - 1]
+            var d1 = dataset[i];
+
+            // work out which date value is closest to the mouse
+            var d = mouseDate - d0[0] > d1[0] - mouseDate ? d1 : d0;
+
+            //draw tool tip div
+            toolTipValues();
+
+            //to align it with the transformed svg
+            hoverLine.attr("transform", "translate(" + xScale(d.date) + ",0)");
+            hoverLineGroup.style("opacity", 1);
+
+            function toolTipValues() {
+                //define the tooltip coordinates
+                toolTip
+                    .attr("transform", "translate(" + xScale(d.date) + ",0)")
+                    .style('left', d3.event.clientX + 10 + 'px')
+                    .style('top', height - 200 + 'px')
+                    .style('opacity', 0.97);
+
+                toolTip.select('.year').html('Energy consumption in ' + formatDate(mouseDate));
+
+                //add all current data values to the tooltip
+                for (var i = 0; i < propertyNames.length; i++) {
+                    var pName = propertyNames[i];
+                    var property;
+
+                    switch (propertyNames[i]) {
+                        case "oilCons":
+                            property = "Oil";
+                            break;
+                        case "coalCons":
+                            property = "Coal";
+                            break;
+                        case "gasCons":
+                            property = "Gas";
+                            break;
+                        case "hydroCons":
+                            property = "Hydro";
+                            break;
+                        case "solarCons":
+                            property = "Solar";
+                            break;
+                        case "windCons":
+                            property = "Wind";
+                            break;
+                    }
+
+                    toolTip.select('.' + propertyNames[i])
+                        .html(property + ': ' + Math.round(d[pName] * 100) / 100)
+                        .style('color', colors[i]);
+                }
+
+            }
+
+        }
+
+        function mouseout() {
+            hoverLineGroup.style("opacity", 1e-6);
+            toolTip.style('opacity', 0);
+        }
+
+        drawLegend();
+
+        function drawLegend() {
+            var size = 13
+            // Add one rect in the legend for each name
+            svg.selectAll("labelRects")
+                .data(colors)
+                .enter()
+                .append("rect")
+                .attr("x", 582)
+                .attr("y", function (d, i) { return 5 + i * (size + 5) }) // 5 is where the first rectangle appears
+                .attr("width", size)
+                .attr("height", size)
+                .style("fill", function (d) { return d })
+                .data(propertyNames)
+                .attr("class", function (d) {
+                    return "legendRects " + d + "Rect";
+                })
+                .on("click", legendOnclick)
+
+
+            // Add text for each rectangle in the legend
+            svg.selectAll("mylabels")
+                .data(propertyNames)
+                .enter()
+                .append("text")
+                .attr("x", 582 + size * 1.2)
+                .attr("y", function (d, i) { return 5 + i * (size + 5) + (size / 2) })
+                .style("fill", function (d) { return colors[0] })
+                .text(function (d) {
+                    switch (d) {
+                        case "oilCons":
+                            property = "Oil";
+                            break;
+                        case "coalCons":
+                            property = "Coal";
+                            break;
+                        case "gasCons":
+                            property = "Gas";
+                            break;
+                        case "hydroCons":
+                            property = "Hydro";
+                            break;
+                        case "solarCons":
+                            property = "Solar";
+                            break;
+                        case "windCons":
+                            property = "Wind";
+                            break;
+                    }
+                    return property;
+                })
+                .attr("text-anchor", "left")
+                .style("alignment-baseline", "middle")
+                .style("font-size", "12.7px")
+                .data(propertyNames)
+                .attr("class", function (d) { return "legendText " + d + "Text"; })
+                .on("click", legendOnclick)
+                .on("blur", drawAllLines)
+
+
+            // a function that will be called when either the rectangle or text of a legend is clicked
+            function legendOnclick() {
+
+                var lineOpacity = 0.05;
+                var nodeClicked = this;
+                drawAllLines();
+
+                if (nodeClicked.classList.contains('coalConsText') || nodeClicked.classList.contains('coalConsRect')) {
+                    //reduce the opacity of the other lines
+                    svg.selectAll(".consLines:not(.coalConsLine)")
+                        .style("opacity", lineOpacity);
+                }
+                else if (nodeClicked.classList.contains('gasConsText') || nodeClicked.classList.contains('gasConsRect')) {
+                    //reduce the opacity of the other lines
+                    svg.selectAll(".consLines:not(.gasConsLine)")
+                        .style("opacity", lineOpacity);
+                }
+                else if (nodeClicked.classList.contains('oilConsText') || nodeClicked.classList.contains('oilConsRect')) {
+                    //reduce the opacity of the other lines
+                    svg.selectAll(".consLines:not(.oilConsLine)")
+                        .style("opacity", lineOpacity);
+                }
+                else if (nodeClicked.classList.contains('hydroConsText') || nodeClicked.classList.contains('hydroConsRect')) {
+                    //reduce the opacity of the other lines
+                    svg.selectAll(".consLines:not(.hydroConsLine)")
+                        .style("opacity", lineOpacity);
+                }
+                else if (nodeClicked.classList.contains('windConsText') || nodeClicked.classList.contains('windConsRect')) {
+                    //reduce the opacity of the other lines
+                    svg.selectAll(".consLines:not(.windConsLine)")
+                        .style("opacity", lineOpacity);
+                }
+                else if (nodeClicked.classList.contains('solarConsText') || nodeClicked.classList.contains('solarConsRect')) {
+                    //reduce the opacity of the other lines
+                    svg.selectAll(".consLines:not(.solarConsLine)")
+                        .style("opacity", lineOpacity);
+                }
+                else {
+                    drawAllLines();
+                }
+
+            }
+
+            
+        }
+
 
     }
 
 }
 
 window.onload = init;
+
